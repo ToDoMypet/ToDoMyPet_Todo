8105f5dbab6295da586751b6414206ec9452c082
dawonseo <oloielo@naver.com>
2024-02-21 14:33:38 +0900

    chore: alert type 수정

--- src/main/java/com/todomypet/todoservice/domain/node/AlertType.java
+++ src/main/java/com/todomypet/todoservice/domain/node/AlertType.java
@@ -4,11 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum AlertType {
-    EXACT("정각"),
+    EVENT_TIME("이벤트 시간"),
     FIVE_MINUTE("5분 전"),
     TEN_MINUTE("10분 전"),
+    FIFTEEN_MINUTE("15분 전"),
+    THIRTY_MINUTE("30분 전"),
     ONE_HOUR("1시간 전"),
-    ONE_DAY("하루 전")
+    TWO_HOUR("2시간 전"),
+    ONE_DAY("1일 전"),
+    TWO_DAY("2일 전"),
+    ONE_WEEK("1주 전")
     ;
 
     private final String alertType;
