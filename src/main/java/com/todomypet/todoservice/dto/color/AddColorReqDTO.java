3aa6924a279c75f5229a2ab854c6a9b725933a38
dawonseo <oloielo@naver.com>
2024-03-13 13:52:26 +0900

    chore: 어노테이션 추가

--- src/main/java/com/todomypet/todoservice/dto/color/AddColorReqDTO.java
+++ src/main/java/com/todomypet/todoservice/dto/color/AddColorReqDTO.java
@@ -1,10 +1,14 @@
 package com.todomypet.todoservice.dto.color;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddColorReqDTO {
     private String colorCode;
     private String bgCode;
--- src/main/java/com/todomypet/todoservice/dto/color/AddColorReqListDTO.java
+++ src/main/java/com/todomypet/todoservice/dto/color/AddColorReqListDTO.java
@@ -1,12 +1,16 @@
 package com.todomypet.todoservice.dto.color;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddColorReqListDTO {
     private List<AddColorReqDTO> colorList;
 }
